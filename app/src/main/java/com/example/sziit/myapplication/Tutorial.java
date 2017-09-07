803772f465664e80c33d5592c08ec1202561420d
szvone <admin@szvone.cn>
2017-09-07 23:49:38 +0800

    2

---
+++ app/src/main/java/com/example/sziit/myapplication/Tutorial.java
@@ -0,0 +1,13 @@
+package com.example.sziit.myapplication;
+
+/**
+ * Created by sziit on 2017/9/7.
+ */
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+public class Tutorial implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
